803ca98157449b2cb6ac49fbcffb5b102b0b3702
haribo52 <alexeizubtsoff@yandex.ru>
2024-11-07 16:54:14 +0400

    refactor code

--- main-service/src/main/java/ru/practicum/dto/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/dto/mainservice/MainServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum.client.client", "ru.practicum.dto.mainservice"})
+@ComponentScan(basePackages = {"ru.practicum.dto.mainservice","ru.practicum.client.client"})
 public class MainServiceApplication {
 
     public static void main(String[] args) {
