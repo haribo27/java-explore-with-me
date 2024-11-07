75cf98e01c270730de5f7fe828242911b36a7cf1
haribo52 <alexeizubtsoff@yandex.ru>
2024-11-07 16:27:01 +0400

    checkstyle

--- stats-service/client/src/main/java/ru/practicum/client/client/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/client/client/StatsClient.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
