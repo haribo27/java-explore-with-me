5eec21554d63134dc360546578384fbbd875017e
haribo52 <alexeizubtsoff@yandex.ru>
2024-11-07 16:23:28 +0400

    checkstyle

--- stats-service/dto/src/main/java/ru/practicum/dto/HitRequestDto.java
+++ stats-service/dto/src/main/java/ru/practicum/dto/HitRequestDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
